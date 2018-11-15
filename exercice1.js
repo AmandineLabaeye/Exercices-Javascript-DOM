7851ed646bf14d8c04913305bb9978af305c4c23
Amandine <amandine.labaeye@orange.fr>
2018-11-15 10:56:04 +0100

    commit

--- exercice1.js
+++ exercice1.js
@@ -3,14 +3,27 @@
  */
 
 
-var monDiv = .createElement('div');
+var monDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
 
+monDiv.innerHTML = "Ce div a été ajouté en utilisant du javascript";
 
 
 
+var MonDeuxiemeDiv = document.createElement('div');
+
+document.getElementById('viewport').appendChild(MonDeuxiemeDiv);
+
+MonDeuxiemeDiv.style.backgroundColor = 'red';
+
+MonDeuxiemeDiv.style.width = '100px';
+
+MonDeuxiemeDiv.style.height = '100px';
 
 
 
+var Supprime = document.getElementById('aSupprimer');
+
+document.getElementById('viewport').removeChild(Supprime);
 
